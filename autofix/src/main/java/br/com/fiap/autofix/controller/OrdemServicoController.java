b1e6fc569905725594937185361bbf94ba0fc797
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-04-06 02:44:57 -0300

    Adicionado CORS

--- autofix/src/main/java/br/com/fiap/autofix/controller/OrdemServicoController.java
+++ autofix/src/main/java/br/com/fiap/autofix/controller/OrdemServicoController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 import br.com.fiap.autofix.model.OrdemServico;
 
 @RestController
+@CrossOrigin
 public class OrdemServicoController {
 
     private Logger log = LoggerFactory.getLogger(getClass());
